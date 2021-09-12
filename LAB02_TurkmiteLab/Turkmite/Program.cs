f5dcd37565edd19f221522a0b55c01494c5ae4cf
Roland Bata <magmakocka9@gmail.com>
2021-09-12 21:20:05 +0200

    Feladat 5: Osztályba kiszervezés, elírt namespace átírása.

--- LAB02_TurkmiteLab/Turkmite/Program.cs
+++ LAB02_TurkmiteLab/Turkmite/Program.cs
@@ -1,44 +1,14 @@
 ﻿using OpenCvSharp;
-using System;
 
 namespace TurkMite
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Mat img = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
-            var indexer = img.GetGenericIndexer<Vec3b>();
-            int x = 100;
-            int y = 100;
-            int direction = 0;  // 0 up, 1 right, 2 down, 3 left
-            Vec3b black = new Vec3b(0, 0, 0);
-            Vec3b white = new Vec3b(255, 255, 255);
-            for(int i=0; i<13000; i++)
-            {
-                Vec3b currentColor = indexer[y, x];
-                if (currentColor == black)
-                {
-                    indexer[y, x] = white;
-                    direction++;
-                }
-                else
-                {
-                    indexer[y, x] = black;
-                    direction--;
-                }
-
-                direction = (direction + 4) % 4;
-
-                var delta = new (int x, int y)[] { (0,-1), (1, 0) , (0, 1) , (-1, 0)};
-                x += delta[direction].x;
-                y += delta[direction].y;
-
-                x = Math.Max(0, Math.Min(x, img.Cols - 1));
-                y = Math.Max(0, Math.Min(y, img.Rows - 1));
-
-            }
-            Cv2.ImShow("TurkMite", img);
+            var turkmite = new TurkMite();
+            turkmite.Run();
+            Cv2.ImShow("TurkMite", turkmite.Image);
             Cv2.WaitKey();
         }
     }
---
+++ LAB02_TurkmiteLab/Turkmite/TurkMite.cs
@@ -0,0 +1,55 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurkMite
+{
+    class TurkMite
+    {
+        private int x = 100;
+        private int y = 100;
+        private int direction = 0;  // 0 up, 1 right, 2 down, 3 left
+        //this will be constant because it will never modified.
+        private readonly Vec3b black = new Vec3b(0, 0, 0);
+        private readonly Vec3b white = new Vec3b(255, 255, 255);
+
+        public Mat Image { get; }
+        private Mat.Indexer<Vec3b> indexer;
+        public TurkMite()
+        {
+            Image = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
+            indexer = Image.GetGenericIndexer<Vec3b>();
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 13000; i++)
+            {
+                Vec3b currentColor = indexer[y, x];
+                if (currentColor == black)
+                {
+                    indexer[y, x] = white;
+                    direction++;
+                }
+                else
+                {
+                    indexer[y, x] = black;
+                    direction--;
+                }
+
+                direction = (direction + 4) % 4;
+
+                var delta = new (int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
+                x += delta[direction].x;
+                y += delta[direction].y;
+
+                x = Math.Max(0, Math.Min(x, Image.Cols - 1));
+                y = Math.Max(0, Math.Min(y, Image.Rows - 1));
+
+            }
+        }
+    }
+}
