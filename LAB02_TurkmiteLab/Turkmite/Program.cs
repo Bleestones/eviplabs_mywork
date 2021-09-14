a9b521aede83a4a5c4b08f96f52df105185ff394
Roland Bata <magmakocka9@gmail.com>
2021-09-14 11:43:01 +0200

    LabTurkmite added, 3 colours, with internal counter

--- LAB02_TurkmiteLab/Turkmite/Program.cs
+++ LAB02_TurkmiteLab/Turkmite/Program.cs
@@ -8,7 +8,7 @@ namespace TurkMite
         {
             Mat img = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
             //var turkmite = new OriginalTurkmite(img);
-            var turkmite = new ThreeColorTurkmite(img);
+            var turkmite = new LabTurkmite(img);
             for(int i = 0; i < turkmite.PreferredIterationCount; i++)
                 turkmite.Step();
             Cv2.ImShow("TurkMite", turkmite.Image);
