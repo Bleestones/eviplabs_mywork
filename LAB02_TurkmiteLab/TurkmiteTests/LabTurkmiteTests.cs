392637556faa270d3b86c13af8739e9b381caa47
Roland Bata <magmakocka9@gmail.com>
2021-09-14 12:05:52 +0200

    First test added.  Will be finished.

---
+++ LAB02_TurkmiteLab/TurkmiteTests/LabTurkmiteTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using TurkMite;
+using OpenCvSharp;
+
+namespace LabTurkmiteTests
+{
+    public class LabTurkmiteTests
+    {
+        TestLabTurkmite turkmite = new TestLabTurkmite(new Mat(10, 10, MatType.CV_8UC3, new Scalar(0, 0, 0)));
+
+        [Fact]
+        public void BlackFieldAndCounterCorrectThenColorYellow()
+        {
+            var result = turkmite.GetNextColorAndUpdateDirection(turkmite.Black);
+            Assert.Equal(turkmite.Yellow, result.newColor);
+            Assert.Equal(3, result.deltaDirection);
+        }
+
+        private class TestLabTurkmite : LabTurkmite
+        {
+            public Vec3b Black { get { return black; } }
+            public Vec3b Red { get { return red; } }
+            public Vec3b Yellow { get { return yellow; } }
+
+            public new (Vec3b newColor, int deltaDirection) GetNextColorAndUpdateDirection(Vec3b currentColor)
+            {
+                return (base.GetNextColorAndUpdateDirection(currentColor));
+            }
+
+            public TestLabTurkmite(Mat image) : base(image)
+            {
+            }
+        }
+    }
+}
