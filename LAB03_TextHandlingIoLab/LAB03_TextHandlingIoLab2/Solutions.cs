7d2d00276d3c4eb45fa4c77e86bdf548f6f1dfd3
Roland Bata <magmakocka9@gmail.com>
2021-09-21 09:28:57 +0200

    Regex-k constantá változtatása

--- LAB03_TextHandlingIoLab/LAB03_TextHandlingIoLab2/Solutions.cs
+++ LAB03_TextHandlingIoLab/LAB03_TextHandlingIoLab2/Solutions.cs
@@ -7,13 +7,13 @@ namespace LAB03_TextHandlingIoLab2
 {
     class Solutions
     {
-        private string emailRegex = @"[a-z.]+\@[a-z.]+[a-z]+";
-        private string phoneRegex = @"\+\d-(\d{3})+-(\d{3})+-(\d{4})+|(\+36|06)(\d{8,9}|-\d{2}-\d{3}-\d{4})";
-        private string phoneHungaryRegex = @"(\+36|06)(\d{2})(\d{7})|(\+36|06)(\d{2})-(\d{3})-(\d{4})";
-        private string musicboxRegex = @"([a-zA-Zóőáéüö]+)?(dó|ré|mi|fá|szó|lá|ti)([a-zA-zóőáéüö]+)?";
-        private string pluscodeRegex = @"(([23456789C][23456789CFGHJMPQRV][23456789CFGHJMPQRVWX]{6}\+[23456789CFGHJMPQRVWX]{2,3}))";
-        private string pluscodeBudapestRegex = @"([23456789CFGHJMPQRVWX]{4}\+[23456789CFGHJMPQRVWX]{2,3})( [a-zA-z]+)";
-        private string dateRegex = @"([0-9]){4}[-|\/](0[1-9]|1[0-2])[-|\/](0[1-9]|[12][0-9]|3[01])";
+        private const string emailRegex = @"[a-z.]+\@[a-z.]+[a-z]+";
+        private const string phoneRegex = @"\+\d-(\d{3})+-(\d{3})+-(\d{4})+|(\+36|06)(\d{8,9}|-\d{2}-\d{3}-\d{4})";
+        private const string phoneHungaryRegex = @"(\+36|06)(\d{2})(\d{7})|(\+36|06)(\d{2})-(\d{3})-(\d{4})";
+        private const string musicboxRegex = @"([a-zA-Zóőáéüö]+)?(dó|ré|mi|fá|szó|lá|ti)([a-zA-zóőáéüö]+)?";
+        private const string pluscodeRegex = @"(([23456789C][23456789CFGHJMPQRV][23456789CFGHJMPQRVWX]{6}\+[23456789CFGHJMPQRVWX]{2,3}))";
+        private const string pluscodeBudapestRegex = @"([23456789CFGHJMPQRVWX]{4}\+[23456789CFGHJMPQRVWX]{2,3})( [a-zA-z]+)";
+        private const string dateRegex = @"([0-9]){4}[-|\/](0[1-9]|1[0-2])[-|\/](0[1-9]|[12][0-9]|3[01])";
 
         #region Examples and helper
         internal bool MatchingExample()
