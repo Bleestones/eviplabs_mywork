dedba8f64c052b6bff0a5cc94d7eceed6ce32e47
Roland Bata <magmakocka9@gmail.com>
2021-09-25 16:53:02 +0200

    Növény osztály megírása

---
+++ LAB05_CollectionsAndGenerics/Storage/Plant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public class Plant : IStorable
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public string Genre { get; set; }
+        public int InStock { get; set; }
+
+
+        public Plant(string id, int stock, string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
+            Id = id;
+            InStock = stock;
+        }
+
+        public Plant()
+        {
+
+        }
+        public override string ToString()
+        {
+            return Id + ": '" + Title + "' genre " + Genre + " - Available: " + InStock;
+        }
+    }
+}
