5429067cc40d80a4abbfd47e6c61d1d90e88eca8
Roland Bata <magmakocka9@gmail.com>
2021-10-08 16:11:36 +0200

    Csúszka és szövegdoboz megírása

--- LAB07_UWP_Basics/01/UWP_Basics/UWP_Basics/MainPage.xaml.cs
+++ LAB07_UWP_Basics/01/UWP_Basics/UWP_Basics/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Text.RegularExpressions;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,6 +27,8 @@ namespace UWP_Basics
         {
             this.InitializeComponent();
             stackPanel.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            firstRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+            sliderTextBox.Text = slider.Value.ToString();
         }
 
         private void colorBtn_Click(object sender, RoutedEventArgs e)
@@ -41,7 +44,7 @@ namespace UWP_Basics
             {
                 case 0:
                     {
-                        stackPanel.Background = new SolidColorBrush(Windows.UI.Colors.DarkOrange);
+                        stackPanel.Background = new SolidColorBrush(Windows.UI.Colors.DarkSlateBlue);
                         changed = 1;
                         break;
                     }
@@ -68,6 +71,52 @@ namespace UWP_Basics
         {
             firstTextBox.IsEnabled = false;
             seconTextBox.IsEnabled = false;
+        }
+
+        private byte rectangleColorSwitch = 2;
+        private void switchButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch(rectangleColorSwitch)
+            {
+                case 1:
+                    {
+                        firstRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+                        thirdRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+                        rectangleColorSwitch++;
+                        break;
+                    }
+                case 2:
+                    {
+                        firstRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+                        secondRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+                        rectangleColorSwitch++;
+                        break;
+                    }
+                case 3:
+                    {
+                        secondRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+                        thirdRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+                        rectangleColorSwitch = 1;
+                        break;
+                    }
+            }
+        }
+
+        private void slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            sliderTextBox.Text = slider.Value.ToString();
+        }
+
+        private void sliderTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Regex.IsMatch(sliderTextBox.Text, "[^0-9]"))
+            {
+                sliderTextBox.Text = sliderTextBox.Text.Remove(sliderTextBox.Text.Length - 1);
+            }
+            else if(Regex.IsMatch(sliderTextBox.Text, "[0-9]"))
+            {
+                slider.Value = double.Parse(sliderTextBox.Text);
+            }
         }
     }
 }
