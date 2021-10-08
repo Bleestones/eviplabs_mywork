02af7a2154642f6234f2ea6481d8eaf09c12fc79
Roland Bata <magmakocka9@gmail.com>
2021-10-08 22:51:33 +0200

    Még módosítom

--- LAB07_UWP_Basics/01/UWP_Basics/UWP_Basics/MainPage.xaml.cs
+++ LAB07_UWP_Basics/01/UWP_Basics/UWP_Basics/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Text.RegularExpressions;
+using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,13 +24,83 @@ namespace UWP_Basics
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public Button[,] buttons = new Button[3, 3];
         public MainPage()
         {
             this.InitializeComponent();
             stackPanel.Background = new SolidColorBrush(Windows.UI.Colors.Black);
             firstRectangle.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
             sliderTextBox.Text = slider.Value.ToString();
+            ButtonMatrixInit();
         }
+
+        private void ButtonMatrixInit()
+        {
+            for (int y = 0; y <= buttons.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x <= buttons.GetUpperBound(0); x++)
+                {
+                    buttons[y, x] = new Button()
+                    {
+                        Name = "matrixButton"+y+x,
+                        Width = 150,
+                        Content = " ",
+                        Background = new SolidColorBrush(Windows.UI.Colors.DarkGray),
+                    };
+                    Grid.SetRow(buttons[y, x], x);
+                    Grid.SetColumn(buttons[y, x], y);
+                    buttons[y, x].Click += Button_Click;
+                    matrixButtonGrid.Children.Add(buttons[y, x]);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            //Color buttonBrush = (SolidColorBrush)button.Background;
+            //if ((SolidColorBrush)button.Background == Colors.DarkGreen)
+            //{
+            //    button.Background = new SolidColorBrush(Windows.UI.Colors.DarkGreen);
+            //}
+            //else if(buttonBrush == new SolidColorBrush(Windows.UI.Colors.DarkGreen))
+            //{
+            //    button.Background = new SolidColorBrush(Windows.UI.Colors.DarkGray);
+            //}
+            //SetOtherButtonsColor(button.Name.ToString(), button.Background);
+        }
+
+        private void SetOtherButtonsColor(string getButtonName, Brush buttonColor)
+        {
+            var indexesOfButton = getButtonIndex(getButtonName);
+            if(buttonColor.Equals(new SolidColorBrush(Windows.UI.Colors.DarkGreen)))
+            {
+                SetButtonColor(Windows.UI.Colors.DarkGray, indexesOfButton);
+            }
+            else
+            {
+                SetButtonColor(Windows.UI.Colors.DarkGreen, indexesOfButton);
+            }
+        }
+
+        private void SetButtonColor(Color darkGray, Tuple<int, int> indexesOfButton)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Tuple<int, int> getButtonIndex(string buttonName)
+        {
+            for(int y = 0; y <= buttons.GetUpperBound(1); y++)
+            {
+                for(int x = 0; x <= buttons.GetUpperBound(0); x++)
+                {
+                    if (buttons[y, x].Name.ToString().Equals(buttonName))
+                        return Tuple.Create(x, y);
+                }
+            }
+            return null;
+        }
+
 
         private void colorBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -125,7 +196,7 @@ namespace UWP_Basics
             {
                 Name = "deleteButton",
                 Content = "Delete me",
-                Width = 100      
+                Width = 100
             };
             deleteButton.Click += Btn_Click; //feliratkozik
             stackPanel.Children.Add(deleteButton);
