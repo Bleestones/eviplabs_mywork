f2a0b9900e14995098b55639b95b43ca7a3bbc08
Roland Bata <magmakocka9@gmail.com>
2021-10-19 10:42:37 +0200

    Szöveg generálás

---
+++ ZH1/Common/SquareGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class SquareGenerator : ITextSequenceSource
+    {
+        int N;
+        public SquareGenerator(int N)
+        {
+            this.N = N;
+        }
+        public IEnumerable<string> GenerateTexts()
+        {
+            for(int i = 0; i < N; i++)
+            {
+                int gyok = (int)Math.Sqrt(i);
+                if (gyok * gyok == i)
+                    yield return $"Az {i} négyzetszám";
+            }
+        }
+    }
+}
