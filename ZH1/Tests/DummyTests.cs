2dd68f713cf49882fa68bc77a0610ceb50ce245f
Roland Bata <magmakocka9@gmail.com>
2021-10-19 10:30:51 +0200

    Testsben triviális unit teszt létrehozása

---
+++ ZH1/Tests/DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using Common;
+
+namespace Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            var dummyGenerator = new DummyGenerator() { N = 10};
+            Assert.Equal(10, dummyGenerator.GenerateTexts().Where(dummyText => dummyText.Equals("dummy")).Count());
+        }
+    }
+}
